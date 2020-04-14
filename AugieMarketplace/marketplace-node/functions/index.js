4bb844838b2dfe41494a92b16f634499b7bbdfdd
nguyentruong17 <nguyentruong17@augustana.edu>
2020-04-14 15:02:21 -0500

    create and read data from firebase

--- AugieMarketplace/marketplace-node/functions/index.js
+++ AugieMarketplace/marketplace-node/functions/index.js
@@ -1,8 +1,39 @@
 const functions = require('firebase-functions');
-
+const admin = require('firebase-admin')
+admin.initializeApp()
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+
+exports.getItems = functions.https.onRequest((req, res) => {
+    admin.firestore().collection('items').get()
+        .then( data => {
+            let items = []
+            data.forEach(doc => {
+                items.push(doc.data())
+            })
+
+            return res.json(items);
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
+
+exports.createItem = functions.https.onRequest((req, res) => {
+    const newItem = {
+        body: req.body.body,
+        userHandle: req.body.userHandle,
+        createdAt: admin.firestore.Timestamp.fromDate(new Date()) 
+    }
+    admin.firestore()
+        .collection('items')
+        .add(newItem)
+        .then(doc => {
+            res.json({ messeage: `document ${doc.id} created successfully`})
+        })
+        .catch(err => {
+            res.status(500).json({ error: 'server went wrong'})
+            console.error(err)
+        })
+})
---
+++ AugieMarketplace/marketplace-node/readme.md
@@ -0,0 +1,2 @@
+code from this tutorial:  </br>
+https://youtu.be/m_u6P5k0vP0
